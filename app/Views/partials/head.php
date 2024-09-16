3339f609b6dbb0631f01d8d63b73000a3cd21c7c
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-16 16:55:45 +0200

    mise a jour cours

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -32,6 +32,9 @@
         <li class="nav-item">
           <a class="nav-link active" href="/app/Controllers/PdoController.php">PDO</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/app/Controllers/formulaireController.php">Inscription</a>
+        </li>
       </ul>
     </div>
   </div>
