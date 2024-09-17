8e0926cd65acefc6ea23c29f15b7dcf3469bb82d
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-17 09:34:09 +0200

    mise a jours cours

--- app/Controllers/FormulaireController.php
+++ app/Controllers/FormulaireController.php
@@ -19,10 +19,28 @@ isNotEmpty('service');
 isNotEmpty('date_embauche');
 isNotEmpty('salaire');
 
+
+
+function checkFormat($nameInput, $value){
+    global $arrayError;
+    $regexName = '/^[a-zA-Zà-üÀ-Ü \-]{2,255}$/';
+    //Vos regex
+
+    switch ($nameInput) {
+        case 'nom':
+            if(!preg_match($regexName, $value)){
+                $arrayError[] = 'Merci de renseigner un nom correcte';
+            }
+            break;
+    }
+}
+
+
 if(empty($arrayError)){
     $nom = htmlspecialchars($_POST['nom']);
 
-    
+    checkFormat('nom', $nom);
+ 
 }
 
 
