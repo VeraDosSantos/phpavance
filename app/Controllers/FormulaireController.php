d0b936c7c12fbd66dcb24106716326981416d171
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-17 11:52:44 +0200

    fin de la correction

--- app/Controllers/FormulaireController.php
+++ app/Controllers/FormulaireController.php
@@ -61,12 +61,52 @@ function check($nameInput, $value){
 
 }
 
-check('nom', $_POST['nom']);
-check('prenom', $_POST['prenom']);
-check('genre', $_POST['genre']);
-check('service', $_POST['service']);
-check('date_embauche', $_POST['date_embauche']);
-check('salaire', $_POST['salaire']);
+//Connexion à la Base de Données:
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=societe',
+    'root',
+    '',
+    array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,  // pour afficher les messages d'erreur SQL
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'set NAMES utf8'
+    ) 
+);
+
+if(isset($_POST['nom'])){
+    check('nom', $_POST['nom']);
+    check('prenom', $_POST['prenom']);
+    check('genre', $_POST['genre']);
+    check('service', $_POST['service']);
+    check('date_embauche', $_POST['date_embauche']);
+    check('salaire', $_POST['salaire']);
+
+    if(empty($arrayError)){
+        $nom = htmlspecialchars($_POST['nom']);
+        $prenom = htmlspecialchars($_POST['prenom']);
+        $genre = htmlspecialchars($_POST['genre']);
+        $service = htmlspecialchars($_POST['service']);
+        $date = htmlspecialchars($_POST['date_embauche']);
+        $salaire = htmlspecialchars($_POST['salaire']);
+    
+        //je peux envoyer à la BDD
+        $query = 'INSERT INTO `employes` (`nom`, `prenom`, `sexe`, `service`, `date_embauche`, `salaire`)
+                VALUES (:nom, :prenom, :sexe, :service, :date_embauche, :salaire)';
+        $queryStatement = $pdo->prepare($query);
+        $queryStatement->bindValue(':nom', $nom);
+        $queryStatement->bindValue(':prenom', $prenom);
+        $queryStatement->bindValue(':sexe', $genre);
+        $queryStatement->bindValue(':service', $service);
+        $queryStatement->bindValue(':date_embauche', $date);
+        $queryStatement->bindValue(':salaire', $salaire);
+        $queryStatement->execute();
+    
+        header('Location: /');
+    }
+}
+
+
+
+
 
 
 
