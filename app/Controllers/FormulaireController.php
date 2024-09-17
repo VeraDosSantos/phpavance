fb8c5c2fca157c38e7db9bab80915cec1be1ddab
VeraDosSantos <vera.dos-santos@lepoles.org>
2024-09-17 11:15:46 +0200

    correction regex

--- app/Controllers/FormulaireController.php
+++ app/Controllers/FormulaireController.php
@@ -11,37 +11,65 @@ function isNotEmpty($value) {
     return false;
 }
 
-//Verifier si mes champs ne sont pas vide :
-isNotEmpty('nom');
-isNotEmpty('prenom');
-isNotEmpty('genre');
-isNotEmpty('service');
-isNotEmpty('date_embauche');
-isNotEmpty('salaire');
-
-
-
 function checkFormat($nameInput, $value){
     global $arrayError;
     $regexName = '/^[a-zA-Zà-üÀ-Ü \-]{2,255}$/';
+    $regexGenre = '/^[f|m]{1}$/';
+    $regexDate = '/^[1-2][0|9][0-2|5-9][0-9][-][0-1][0-9][-][0-3][0-9]$/';
+    $regexSalaire = '/^[\d.,]{1,}$/';
     //Vos regex
 
     switch ($nameInput) {
         case 'nom':
             if(!preg_match($regexName, $value)){
-                $arrayError[] = 'Merci de renseigner un nom correcte';
+                $arrayError[] = 'Merci de renseigner un nom correcte!';
+            }
+            break;
+        case 'prenom':
+            if(!preg_match($regexName, $value)){
+                $arrayError[] = 'Merci de renseigner un prenom correcte!';
+            }
+            break;
+        case 'genre':
+            if(!preg_match($regexGenre, $value)){
+                $arrayError[] = 'Merci de renseigner un genre correcte!';
+            }
+            break;
+        case 'service':
+            if(!preg_match($regexName, $value)){
+                $arrayError[] = 'Merci de renseigner un service correcte!';
+            }
+            break;
+        case 'date_embauche':
+            if(!preg_match($regexDate, $value)){
+                $arrayError[] = 'Merci de renseigner une date correcte!';
+            }
+            break;
+        case 'salaire':
+            if(!preg_match($regexSalaire, $value)){
+                $arrayError[] = 'Merci de renseigner un salaire correcte!';
             }
             break;
     }
 }
 
+function check($nameInput, $value){
 
-if(empty($arrayError)){
-    $nom = htmlspecialchars($_POST['nom']);
+    isNotEmpty($nameInput);
+    $value = htmlspecialchars($value);
+    checkFormat($nameInput, $value);
 
-    checkFormat('nom', $nom);
- 
 }
+
+check('nom', $_POST['nom']);
+check('prenom', $_POST['prenom']);
+check('genre', $_POST['genre']);
+check('service', $_POST['service']);
+check('date_embauche', $_POST['date_embauche']);
+check('salaire', $_POST['salaire']);
+
+
+
 
 
 
